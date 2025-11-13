fdcee86e353c9abaef5e7c24195ff03cd3f6799d
sonawaneapekshit <apekshitsonawane@gmail.com>
2025-11-13 21:00:26 +0530

    Inital commit for blog clone

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 md:px-8 lg:px-16 xl:px-32'>
+     {/*NAVBAR*/}
+     <Navbar/>
+     {/*BREADCRUMB*/}
+     {/*Introduction*/}
+     {/*Featured Posts*/}
+     {/* Posts List */}
+    </div>
+  )
+}
+
+export default App

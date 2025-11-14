1dc25471dd4efcb73acd4ea71b810d970d71683a
sonawaneapekshit <apekshitsonawane@gmail.com>
2025-11-14 21:22:35 +0530

    Navbar component updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import IKImage from './IKImage';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -10,14 +11,21 @@ const Navbar = () => {
           href=""
           className="flex items-center gap-4 focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-600"
         >
-          <img src="./logo.png" alt="Blog Logo" className="w-8 h-8" />
+          <IKImage
+            src="/logo.png"
+            alt="Blog Logo"
+            lazyLoad={true}
+            className="w-8 h-8"
+            width={32}
+            height={32}
+          />
           <h1>ApekBlog</h1>
         </a>
       </div>
       {/* Mobile Menu*/}
       <div className="md:hidden">
         <button
-          className="text-4xl"
+          className="text-4xl hover:to-blue-600 focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-800"
           onClick={() => {
             setOpen((prev) => !prev);
           }}
@@ -30,21 +38,43 @@ const Navbar = () => {
             open ? 'translate-x-0' : 'translate-x-full'
           } transition-all ease-in-out`}
         >
-          <ul>
-            <li>
-              <a href="/">Home</a>
+          <ul className="flex flex-col justify-center items-center gap-8 xl:gap-12">
+            <li className="">
+              <a
+                className="font-medium text-xl focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-600"
+                href="/"
+              >
+                Home
+              </a>
             </li>
-            <li>
-              <a href="/">Trending</a>
+            <li className="">
+              <a
+                className="font-medium text-xl focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-600"
+                href="/"
+              >
+                Trending
+              </a>
             </li>
-            <li>
-              <a href="/">Most Popular</a>
+            <li className="">
+              <a
+                className="font-medium text-xl focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-600"
+                href="/"
+              >
+                Most Popular
+              </a>
             </li>
-            <li>
-              <a href="/">About</a>
+            <li className="">
+              <a
+                className="font-medium text-xl focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-600"
+                href="/"
+              >
+                About
+              </a>
             </li>
-            <li>
-              <button>Login 👋</button>
+            <li className="">
+              <button className="font-medium text-xl py-2 px-4 rounded-3xl bg-blue-800 text-white hover:to-blue-600 focus:outline-2 focus:outline-offset-2 focus:outline-dashed focus:outline-slate-800">
+                Login 👋
+              </button>
             </li>
           </ul>
         </div>

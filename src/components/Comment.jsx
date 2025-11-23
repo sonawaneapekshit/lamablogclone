b976ad3a11317574e69689020b1601bdf8ecf4db
sonawaneapekshit <apekshitsonawane@gmail.com>
2025-11-23 16:04:25 +0530

    Single post page related component Comments and single comment updated

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import IKImage from './IKImage';
 
 const Comment = () => {
   return (
-    <div>Comment</div>
-  )
-}
+    <div className="mb-8 flex flex-col bg-slate-50 rounded-lg p-4">
+      <div className="flex items-center gap-4">
+        <IKImage
+          className="rounded-full w-10 he-10 object-cover"
+          src="/userImg.jpeg"
+          width="40"
+          height="40"
+        />
+        <span className="font-medium">John Doe</span>
+        <span className="text-sm text-gray-500">2 Days ago</span>
+      </div>
+      <div className='mt-4'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magnam enim velit deleniti libero nulla aut sunt autem incidunt iusto?</p>
+      </div>
+    </div>
+  );
+};
 
-export default Comment
+export default Comment;
